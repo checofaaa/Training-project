5cfd6098720aabb0315e92f8a2808efc3cc97b2d
Sofa <149671410+checofaaa@users.noreply.github.com>
2024-05-06 11:46:14 +0300

    Add files via upload

---
+++ Метрика/script.js
@@ -0,0 +1,19 @@
+
+// let number1; 
+
+// let number2; 
+
+// let enterNumber = () => {
+//     number1 = +prompt('Введите первое число не меньше -32000 и не больше 32000');
+//     number2 = +prompt('Введите второе число не меньше -32000 и не больше 32000');
+// }
+
+// enterNumber();
+
+// if ((number1 < -32000 || number1 > 32000) || (number2 < -32000 || number2 > 32000)) {
+//     alert ('Вы ввели неккоретное число');
+//     enterNumber();
+// } else {
+//     let sum = number1 + number2;
+//     alert (sum);
+// }
